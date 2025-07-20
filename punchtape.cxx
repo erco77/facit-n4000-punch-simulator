f0a261e02917055071cb15b66e119a151dc21c18
Greg Ercolano <erco@seriss.com>
2025-07-19 19:54:36 -0700

    Updated comments, fixed bit pattern docs

--- punchtape.cxx
+++ punchtape.cxx
@@ -2,7 +2,10 @@
 
 // Facit N4000 Punch Tape Simulator
 //
-//     Only handles default raw mode for punching tapes.
+// v1.00 07/19/2025 erco@seriss.com Initial version
+//
+//     Only handles default raw mode for punching 8-channel tapes.
+//
 
 #include <sys/types.h>
 #include <sys/stat.h>
@@ -22,18 +25,20 @@
 
 //    TAPE
 //    ___________________
-//       o o o o o o o       <- 0x01
-//       o o o o o o o       <- 0x02
-//       o o o o o o o       <- 0x04
-//       o o o o o o o       <- 0x08
-//       o o o o o o o       <- 0x10
+//
+//       O O O O O O O       <- 0x80
+//       O O O O O O O       <- 0x40
+//       O O O O O O O       <- 0x20
+//       O O O O O O O       <- 0x10
+//       O O O O O O O       <- 0x08
 //       . . . . . . .       <- sprockets
-//       o o o o o o o       <- 0x20
-//       o o o o o o o       <- 0x40
-//       o o o o o o o       <- 0x80
+//       O O O O O O O       <- 0x04
+//       O O O O O O O       <- 0x02
+//       O O O O O O O       <- 0x01
+//    ____________________
 // 
 
-// SCROLLABLE CANVAS EXAMPLE -- JUST DRAWS AN 'X'
+// Facit simulator class widget
 class FacitCanvas : public Fl_Widget {
     int datasize;
     unsigned char *data;
@@ -41,15 +46,18 @@ class FacitCanvas : public Fl_Widget {
     int xoffset;            // offset for drawing tape (used for scrolling)
     Fl_Slider *slider;      // (on resize, adjusts slider bounds for scrolling)
 public:
+    // CTOR
     FacitCanvas(int X,int Y,int W,int H,const char*L=0) : Fl_Widget(X,Y,W,H,L) {
+        // Private data members
         datasize = 0;
         data = 0;
         tapewidth = 0;
         xoffset = 0;
         slider = 0;
-
+        // Widget init
         color(0x00cdff00);  // light blue tape
     }
+
     // Set the raw data for the facit tape
     //    new_datasize  - size in bytes of the data buffer
     //    new_data      - the raw facit data bytes buffer
@@ -74,6 +82,9 @@ public:
         return xoffset;
     }
 
+    // Set the slider widget
+    //    The class uses this to update the slider's bounds() when window resized
+    //
     void SetSlider(Fl_Slider *w) {
         slider = w;
     }
@@ -88,13 +99,13 @@ public:
         unsigned char *p = data;
         int start_x = 10 - xoffset;	        // starting x offset from left side of tape
         int start_y = 10;		        // starting y offset from top of tape
-
         int px = start_x;		        // running punch hole x position
         for ( int t=0; t<datasize; t++ ) {	// loop thru raw facit data
             // Draw holes in this tape column
             int py = start_y;		        // start at top of tape and walk down thru bits
             if ( px >= 0 && px <= w() ) {       // only draw if within xywh of window
                 for ( int bit=0x80; (bit & 0x00ff); bit >>= 1 ) {	// shift thru bits 0x80 .. 0x01
+
                     // Draw a punch hole
                     if ( *p & bit ) {
                         // Filled circles
@@ -126,10 +137,13 @@ public:
         }
     }
 
+    // Override window resizes
     void resize(int X,int Y,int W,int H) {
-        Fl_Widget::resize(X,Y,W,H);
+        Fl_Widget::resize(X,Y,W,H);         // let window handle resize
+        // If a scroller (Fl_Sldier) is defined, update its bounds with the new size
         if (slider) {
             slider->bounds(0, tapewidth - W);
+            // Clamp slider's value to be within window
             if (slider->value() > slider->maximum()) {
                 slider->value(slider->maximum());
             }
@@ -160,6 +174,7 @@ unsigned char* ReadRawData(const char *filename, int &size) {
     return data;
 }
 
+// Handle slider (scrollbar) movement
 void Slider_CB(Fl_Widget *w, void *userdata) {
     Fl_Slider *slider = (Fl_Slider*)w;
     FacitCanvas *facit = (FacitCanvas*)userdata;
@@ -168,23 +183,30 @@ void Slider_CB(Fl_Widget *w, void *userdata) {
     facit->SetXOffset(slider->value());
 }
 
+// Main program
 int main(int argc, char *argv[]) {
+
+    // Handle command line arguments first
     if ( argc != 2 ) {
         printf("usage: %s punchdata.bin   - raw binary data to send to a Facit N4000\n", argv[0]);
         return 0;
     }
 
-    // Read raw facit data into buffer
+    // Read user's raw facit data into a buffer
+    //     This buffer will be used to draw, and redraw the tape
+    //
     int size = 0;
     unsigned char *buf = ReadRawData(argv[1], size);
     // Determine how long the tape needs to be
     int tapewidth = (size*(HOLE_SIZE+HOLE_SPACING) + (50*2));	// tape length in pixels
     int tapeheight = (HOLE_SIZE*8)+(HOLE_SPACING*8) + 20;       // tape height (including margins)
 
-    // Create gui, assign raw buffer to Facit class simulator
+    // Create GUI, assign raw buffer to Facit class simulator
     Fl_Double_Window win(1200,tapeheight+SLIDER_HEIGHT,"Facit N4000 Punch Simulator");
     FacitCanvas facit(0,0,win.w(),tapeheight);
     facit.SetData(size, buf, tapewidth);		// assign raw data to facit simulator
+
+    // Create a slider to "scroll" (xoffset) the drawing position for the tape
     Fl_Slider slider(0,facit.h(),win.w(),SLIDER_HEIGHT);
     facit.SetSlider(&slider);
     slider.type(1);                          // horiz slider
@@ -192,6 +214,7 @@ int main(int argc, char *argv[]) {
     slider.bounds(0, tapewidth - win.w());
     slider.slider_size(10.0/100.0);          // slider's 'tab' size as fractional size of widget
     slider.callback(Slider_CB, (void*)&facit);
+    // End the window definition, and show() it
     win.end();
     win.resizable(facit);
     win.show();
